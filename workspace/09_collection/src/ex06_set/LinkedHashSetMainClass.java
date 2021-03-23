f3fbd198e9006fbbd6fe61d29472e3eab6a18b3c
Hataeyeong <electro0218@gmail.com>
2021-03-23 15:27:19 +0900

    2021.03.23 update

---
+++ workspace/09_collection/src/ex06_set/LinkedHashSetMainClass.java
@@ -0,0 +1,24 @@
+package ex06_set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+//LinkedHashSet 클래스는 저장된 요소들의 순서가 유지된다.
+public class LinkedHashSetMainClass {
+
+	public static void main(String[] args) {
+		
+		Set<String> set = new LinkedHashSet<String>();
+		
+		set.add("html");
+		set.add("css");
+		set.add("javascript");
+		set.add("html");
+		set.add("css");
+		set.add("javascript");
+		
+		for (String s : set) {
+			System.out.println(s);
+		}
+	}
+
+}
